97d509faae41d01cc69a62c889ef7affa597cf25
Y0USUF <121503882+Y0USUF@users.noreply.github.com>
2025-10-04 16:15:43 +0530

    Update variables.tf

--- Devops-project/terraform/variables.tf
+++ Devops-project/terraform/variables.tf
@@ -1,13 +1,37 @@
-variable "ami_id" {
-  default = "ami-0dee22c13ea7a9a67" 
-}
-
+# EC2 Configuration
 variable "instance_type" {
-  default = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  default = "key_pair"           #replace key_pair with your key
-
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "ubuntu"
 }
 
+variable "instance_count" {
+  description = "Number of EC2 instances for scaling"
+  type        = number
+  default     = 3
+}
+
+# S3 Buckets
+variable "app_bucket_name" {
+  description = "S3 bucket name for storing the application JAR"
+  type        = string
+  default     = "techeazy-app-jar-bucket" # must be globally unique
+}
+
+variable "ec2_logs_bucket_name" {
+  description = "S3 bucket name for EC2 logs"
+  type        = string
+  default     = "techeazy-ec2-logs" # must be globally unique
+}
+
+variable "elb_logs_bucket_name" {
+  description = "S3 bucket name for ELB logs"
+  type        = string
+  default     = "techeazy-elb-logs" # must be globally unique
+}
